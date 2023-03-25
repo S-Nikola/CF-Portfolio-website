ef290d3dcff44c48edc0884cd85c6e3622d85813
Nikola Stefanovski <nikoding32@gmail.com>
2023-03-25 16:07:33 +0100

    enhanced/light mode

--- js/index.js
+++ js/index.js
@@ -2,30 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
   const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
   function switchTheme(e) {
-    var bodyBackground = document.body;
-    var lookWork = document.getElementById("look-work");
-    var myName = document.getElementById("myName");
+  const bodyBackground = document.body;
+  const lookWork = document.getElementById("look-work");
+  const boldText = document.querySelectorAll("b");
+  const textBox = document.querySelectorAll(".text-box");
+  const project = document.querySelectorAll(".project");
+  const tool = document.querySelectorAll(".tool");
 
-      if (e.target.checked) {
-        bodyBackground.classList.toggle("light-mode");
-        lookWork.classList.toggle("dark-link");
-        myName.classList.toggle("dark-name");
-      }
-      else {
-        bodyBackground.classList.remove("light-mode");
-        lookWork.classList.remove("dark-link");
-        myName.classList.remove("dark-name");
-      }    
+  if (e.target.checked) {
+    bodyBackground.classList.toggle("light-mode");
+    lookWork.classList.toggle("dark-link");
+    boldText.forEach(boldText => boldText.classList.toggle("dark-bold"));
+    textBox.forEach(textBox => textBox.classList.toggle("light-text-box"));
+    project.forEach(project => project.classList.toggle("light-text-box"));
+    tool.forEach(tool => tool.classList.toggle("light-tool"));
+  } else {
+    bodyBackground.classList.remove("light-mode");
+    lookWork.classList.remove("dark-link");
+    boldText.forEach(boldText => boldText.classList.remove("dark-bold"));
+    textBox.forEach(textBox => textBox.classList.remove("light-text-box"));
+    project.forEach(project => project.classList.remove("light-text-box"));
+    tool.forEach(tool => tool.classList.remove("light-tool"));
   }
-  toggleSwitch.addEventListener('change', switchTheme, false);
+}
 
-//   const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-
-// hamburger.addEventListener("click", function() {
-//   hamburger.classList.toggle("active");
-//   navMenu.classList.toggle("active");
-// });
+toggleSwitch.addEventListener("change", switchTheme, false);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
