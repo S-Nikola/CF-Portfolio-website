d44ef3fff675b11790e6a77f6bf57f4cb5128bc1
Nikola Stefanovski <nikoding32@gmail.com>
2023-03-27 15:22:53 +0200

    added impressum

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  //Light mode switch
   const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
   function switchTheme(e) {
   const bodyBackground = document.body;
-  const lookWork = document.getElementById("look-work");
+  const link = document.querySelectorAll(".link");
   const boldText = document.querySelectorAll("b");
   const textBox = document.querySelectorAll(".text-box");
   const project = document.querySelectorAll(".project");
@@ -11,25 +13,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
   if (e.target.checked) {
     bodyBackground.classList.toggle("light-mode");
-    lookWork.classList.toggle("dark-link");
     boldText.forEach(boldText => boldText.classList.toggle("dark-bold"));
     textBox.forEach(textBox => textBox.classList.toggle("light-text-box"));
     project.forEach(project => project.classList.toggle("light-text-box"));
     tool.forEach(tool => tool.classList.toggle("light-tool"));
+    link.forEach(link => link.classList.toggle("dark-link"));
   } else {
     bodyBackground.classList.remove("light-mode");
-    lookWork.classList.remove("dark-link");
     boldText.forEach(boldText => boldText.classList.remove("dark-bold"));
     textBox.forEach(textBox => textBox.classList.remove("light-text-box"));
     project.forEach(project => project.classList.remove("light-text-box"));
     tool.forEach(tool => tool.classList.remove("light-tool"));
+    link.forEach(link => link.classList.remove("dark-link"));
   }
 }
-
 toggleSwitch.addEventListener("change", switchTheme, false);
-});
 
-document.addEventListener('DOMContentLoaded', function() {
+// Hamburger menu
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu');
   
